7c835d0b84f24afeaf72f495fe174e952f815ec1
Rod Johnson <johnsonroda@gmail.com>
2025-07-01 12:41:13 +1000

    Update API to use starter

--- src/main/kotlin/com/embabel/coding/CodingAgentApplication.kt
+++ src/main/kotlin/com/embabel/coding/CodingAgentApplication.kt
@@ -15,15 +15,21 @@
  */
 package com.embabel.coding
 
+import com.embabel.agent.config.annotation.EnableAgentShell
 import com.embabel.agent.config.annotation.EnableAgents
 import com.embabel.agent.config.annotation.LoggingThemes
+import com.embabel.agent.config.annotation.McpServers
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableAgents(loggingTheme = LoggingThemes.SEVERANCE)
+@EnableAgents(
+    loggingTheme = LoggingThemes.SEVERANCE,
+    mcpServers = [McpServers.DOCKER, McpServers.DOCKER_DESKTOP],
+)
+@EnableAgentShell
 class CodingAgentApplication
 
 fun main(args: Array<String>) {
